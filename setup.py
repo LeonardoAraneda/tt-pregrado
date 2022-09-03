936bf86c11e6adb4649f5cf7300b4f50039f365a
Juan Pablo Nahuelpán <nahuelpan.ide@gmail.com>
2022-09-02 22:42:05 -0400

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Código del trabajo de título.',
+    author='Juan Pablo Nahuelpán',
+    license='MIT',
+)
